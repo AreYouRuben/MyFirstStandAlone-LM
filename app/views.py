c6dca5c153ba4472ef13a440cab37ba3811ed0fd
Luis Rubén Chávez Carrillo <luisrubenchavez@gmail.com>
2020-09-10 22:25:54 -0600

    Starting building MNIST app

--- app/views.py
+++ app/views.py
@@ -6,3 +6,7 @@ page = Blueprint('page', __name__)
 @page.route('/')
 def index():
     return render_template('index.html', title='Home')
+
+@page.route('/mnist')
+def mnist():
+    return render_template('mnistPage.html', title='Guess the number')
